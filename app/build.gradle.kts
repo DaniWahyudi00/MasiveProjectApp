020de00a7cba0fde4cb49e640745c6fdba68fd60
hykal1 <hykalhakim312@gmail.com>
2024-06-04 17:29:22 +0700

    add alert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -67,4 +66,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
+    implementation ("androidx.compose.ui:ui:1.2.1")
+    implementation ("androidx.compose.material:material:1.2.1")
+    implementation ("androidx.compose.ui:ui-tooling-preview:1.2.1")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.2.1")
 }
