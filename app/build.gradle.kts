ceb8012c552bd84ca3a6b70f3659b7505990715a
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-06-12 19:43:07 +0700

    Connect to Firebase and add Firebase auth SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.parcelize)
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -64,6 +65,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
